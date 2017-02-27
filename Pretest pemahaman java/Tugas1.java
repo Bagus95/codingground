77574a87b02765cf42d102f58c5b248aa2e88cf1
Bagus95 <bs387421@gmail.com>
2017-02-27 11:15:07 +0700

    Created by Coding Ground Client

---
+++ Pretest pemahaman java/Tugas1.java
@@ -0,0 +1,8 @@
+public class Tugas1{
+
+     public static void main(String []args){
+        String nama = "Bagus";
+        String kota = "Jepara";
+        System.out.println(nama + " " + kota);
+     }
+}
